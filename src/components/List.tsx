88ed912cc9a27b0b665d9bfcca00c456a6580d2f
ingridcriistine <ingridrochacristine>
2025-02-14 17:14:24 -0300

    updates

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,22 @@
-"use client"
 interface data {
+    list: string,
     title: string,
-    description: string,
     status: boolean
 }
 
-const List: React.FC<data> = ({}) => {{
+const List: React.FC<data> = ({list, title, status}) => {
     return (
-        <div> 
-
+        <div className=""> 
+            <div>
+                <h3 className="">{list}</h3>
+                <button className="">New task</button>
+            </div>
+            <div>
+                <h3>{title}</h3>
+                <div className={status ? "bg-green-400" : "bg-red-500"}></div>
+            </div>
         <div/>
-
     );
-}}
+}
+
 export default List;
